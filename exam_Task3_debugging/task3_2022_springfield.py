499651541e45a9d478335370c22fc009682d1893
BuildTools <unconfigured@null.spigotmc.org>
2025-08-11 16:11:59 +0800

    2025-08-11

---
+++ exam_Task3_debugging/task3_2022_springfield.py
@@ -0,0 +1,47 @@
+'''
+# Original Code - Do not change
+import math
+while true:
+    dice_list = []
+    for i in range(2):
+        input("Player {}, press enter to roll the three dice".format(i+1))
+        dice = {0}*3
+        for j in range(3):
+            dice[j] = random.randint(0,6)
+        dice_list.eppend(dice)
+        print("Player {}, dice = {}" .format(i,dice))
+    if max(dice_list[0]) > max(dice_list[1]):
+        print("Player 1 wins!")
+    elif max(dice_list[0]) = max(dice_list[1]):
+        print("No winners!")
+    elif:
+        print("Player 2 wins!")
+
+    play = input("Play again? (Y/N): ").isupper()
+    if play == 'N':
+        print("Thank you for playing, goodbye!")
+        break:
+'''
+
+# Answer as below
+import random #1) random
+while True: # 2) True
+    dice_list = []
+    for i in range(2):
+        input("Player {}, press enter to roll the three dice".format(i+1))
+        dice = [0]*3 # 3)[0]
+        for j in range(3):
+            dice[j] = random.randint(1,6) #4(1,6) 0 is not a dice value
+        dice_list.append(dice) #5. append()
+        print("Player {}, dice = {}" .format(i,dice)) #6 i+1
+    if max(dice_list[0]) > max(dice_list[1]):
+        print("Player 1 wins!")
+    elif max(dice_list[0]) == max(dice_list[1]): #7) == 
+        print("No winners!")
+    else: #8) #else
+        print("Player 2 wins!")
+
+    play = input("Play again? (Y/N): ").upper() #isupper() #9 .upper()
+    if play == 'N':
+        print("Thank you for playing, goodbye!")
+        break # 10. remove :
