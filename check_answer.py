40a4211b2f4aa9d2a26cc34f7349c84d1aaaba91
BuildTools <unconfigured@null.spigotmc.org>
2025-08-18 15:26:56 +0800

    2025-08-18

--- check_answer.py
+++ check_answer.py
@@ -1,10 +1,35 @@
-toppings = ['ham', 'cheese', 'lettuce', 'tomatoes']
-top_cost = [1, 0.8, 0.5, 0.5]
-print("Welcome to All-Health Salad Bar!")
-print("Please select your toppings below.")
-ham = int(input("Quantity of ham: "))
-cheese = int(input("Quantity of cheese: "))
-lettuce = int(input("Quantity of lettuce: "))
-tomatoes = int(input("Quantity of tomatoes: "))
-total_cost = ham*top_cost[0] + cheese*top_cost[1] + lettuce*top_cost[2] + tomatoes*top_cost[3]
-print("Total cost: $", total_cost)
+# while True:
+#     x = int(input("PLayer 1, Please input a whole number between 1 and 50 inclusive, for player 2 to guess"))
+#     if x<1 or x>50:
+#         print("You have inputted a invalid number, please try again.")
+#     else:
+#         break
+# MAX_GUESSES = 5
+# guesses = 0
+# while MAX_GUESSES > guesses:
+#     y = int(input("Player 2 ,Please input a whole number between 1 and 50 inclusive"))
+#     if y == x :
+#         print("You have gotten the correct answer")
+#         break
+#     else:
+#         print("You have got the wrong answer")
+#         guesses += 1
+# if guesses > MAX_GUESSES:
+#     print("GAME OVER")
+
+# queue = ["Person1", "Person2", "Person3", "Person4", "Person5"]
+# rotate = int(input("How may time to roate? : "))
+# person = queue[0]
+# for i in range(rotate+1):
+#     queue.remove(person)
+#     queue.insert(i,person)
+# print(queue)
+
+queue = ["Person1", "Person2", "Person3", "Person4", "Person5"]
+rotate = int(input("How many times to rotate? : "))
+
+for i in range(rotate):
+    first_person = queue.pop(0)   # remove first person
+    queue.append(first_person)    # add them to the back
+
+print(queue)
---
+++ exam_Task4_development/bvss_2022.py
@@ -0,0 +1,160 @@
+# Task 4 
+# You have been asked to write a program for a teacher to enter 
+# the students' names and their respective test marks. 
+# The test marks are whole numbers only. 
+
+# The program must: 
+# (1) allow the user to enter student's name and mark and store 
+# the input in 2 different lists. 
+# The program does not need to validate student's name. 
+# However, it must ask for another mark each time 
+# the user enters a mark that is not between 0 and 100 inclusive. 
+
+# (2) ask the user if there are any more students' 
+# details to be input. The program does not need to validate this input. 
+
+# (3) calculate the average mark. 
+
+# (4) calculate the name of the student with the highest score. 
+
+################## Expected Output ##############
+# When all the input has been entered, the program must display: 
+# •	"There are [insert student count] students". 
+#   o	The [insert student count] must be replaced with the number of students entered. 
+
+# •	“The average mark is [insert average]”. 
+#   o	The [insert average] must be replaced with the average mark, rounded to 1 decimal place. 
+
+# •	[insert student name] scored the highest score of [insert highest score]
+#   o	The [insert student name] is replaced with the name of the student 
+#       with the highest score and [insert highest score] is replaced with the highest score.
+
+# The program must include appropriate input and output messages. 
+# All variables must be appropriately named and suitable comments 
+# inserted to explain the algorithm. [4]
+
+########################################################
+# Task 4.1
+# Write a program to meet the requirements. Save your program as 
+# MARKS_2022_<your name>_<index number>.py [12] 
+########################################################
+# *** Write your code for Task 4.1 here ***
+
+
+namelist = []
+scorelist = []
+totalsum = 0
+average = 0
+maxscore = 0
+maxname = ""
+
+while True:
+    name = input("Enter the student's name: ")
+
+    while True:
+        score = input(f"Enter the score for {name}: ")
+        if score.isdigit():
+            score = int(score)
+            if score >= 0 and score <= 100:
+                break
+            else:
+                print(f"You entered {score}, but score must be between 0 and 100.")
+        else:
+            print(f"You entered {score}, but score must be a valid number.")
+    
+    # all valid, so add to list
+    namelist.append(name)
+    scorelist.append(score)
+    print(f"{name} scored {score}")
+    # print(namelist) # testing values
+    # print(scorelist)
+
+    #### calculating average and max
+    totalsum = totalsum + score
+    if score > maxscore:
+        maxscore = score
+        maxname = name
+
+    proceed = input("Press y to continue or any other key to stop: ")
+    if proceed.lower() != "y":
+        print("Adding of score is done...")
+
+        # showing required output messages
+        # "There are [insert student count] students
+        print(f"There are {len(namelist)} students.")
+
+        # The average mark is [insert average]
+        average = round(totalsum / len(namelist), 1)
+        print(f"The average mark is {average}.")
+
+        # 	[insert student name] scored the highest score of [insert highest score]
+        print(f"{maxname} scored the highest score of {maxscore}")
+
+
+        break
+    else:
+        print("\n" + "*"*15)
+        print("Next student >> ")
+
+
+
+
+
+
+########################################################
+# Task 4.2
+# Copy your program from Task 4.1 into another code cell. 
+# A student who scored lower than the average mark (rounded down) 
+# will need to attend remedial class. 
+# The teacher wants to arrange these students into groups of three. 
+# If the number of students is not a multiple of 3, 
+# the last group will have fewer than 3 students. 
+# Each group will then be assigned a peer tutor. 
+
+# Extend your program to: 
+# •	store the names and score of students who need to attend remedial class in a dictionary 
+# •	include an appropriate output message and display the names of these students. 
+# If no students have been identified, display "There are no students for remedial". 
+
+# •	calculate and display with appropriate output message the number of peer tutors needed if there are students identified for remedial. 
+
+# Save your program. [9] 
+########################################################
+# *** Copy your code from Task 4.1 here and continue *** 
+import math
+
+remedial_students = {}
+
+# add to dictionary
+for i in range(len(namelist)):
+    # average has to be floored down
+    if scorelist[i] < math.floor(average):
+        rstudent = namelist[i]
+        rscore = scorelist[i]
+
+        remedial_students[rstudent] = rscore
+
+total_remedial_students = len(remedial_students)
+
+if total_remedial_students > 0:
+
+    # loop through dictionary to display
+    for student,score in remedial_students.items():
+        print(f"{student} with score of {score} needs to attend remedial.")
+
+    # use floor division to find number of full groups
+    groups = total_remedial_students // 3
+    print(f"There are {groups} full group of 3 students")
+
+    # find any leftover students
+    leftovers = len(remedial_students) % 3
+
+    if leftovers > 0:
+        print(f"There is 1 group with less than 3 students")
+        groups += 1
+
+    print(f"There is a total of {groups} groups, and {groups} peer tutors are needed.")
+
+else:
+    print("There are no students for remedial.")
+
