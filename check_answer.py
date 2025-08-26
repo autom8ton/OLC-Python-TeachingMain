87db8da5b04d6794973cdbeee5aff14cdb589aa6
DLEE <90884127+autom8ton@users.noreply.github.com>
2025-08-26 11:13:59 +0000

    2025-08-26

--- check_answer.py
+++ check_answer.py
@@ -1,59 +1,47 @@
-names = []
-marks = []
-highest_pos = 0
-highest_name = ""
-while True:
-  name = input("Student's name: ")
-  names.append(name)
-  while True:
-    mark = int(input("Student's mark: "))
-    if 0 <= mark <= 100:
-      marks.append(mark)
-      break
-    else:
-      print("Invald score. Enter number from 0 to 100.")
-  
-#   if mark > highest_pos:
-#     highest_pos = mark
-#     highest_name = name
+# this is a backup of the original code.
+# age = 0
+# height = float(0) 
+# rejected = 100
+# rider = 0
+# age = int(input("Please enter your age ))
+# height = float(input("Please enter your height ")) 
+# while age <> 0 and height != 0:
+#     if age < 7 or age > 70 or height <= 1.3: 
+#         if age < 7:
+#             print("You are too young to ride") 
+#         if age > 90:
+#             print("You are too old to ride") 
+#         if height <= 1.3:
+#             print("You are too short to ride") 
+#         rejected = rejected - 1
+# else:
+#         print("You can ride the Roller Coaster") 
+#         rider = Rider + 1
+#     age = int(input("Please enter your age "))
+#     height = float(input("Please enter your height ")) 
+# print("Number of people rejected ", rider) 
+# print("Number of riders ", rejected)
 
-  moreinput = input("Do you wish to add more details? (Y/N): ")
-  if moreinput.upper() == 'N':
-    break
-student_count = len(names)
-avrg = sum(marks)/student_count
-
-highest = 0
-for i in marks:
-  if i > highest:
-    highest = i
-highest_pos = marks.index(highest)
-highest_name = names[highest_pos]
-
-print(f"There are {student_count} students.")
-print(f"The average mark is {avrg}.") # round to 1 decimal place
-print(f"{highest_pos} scored the highest score of {highest}.")
-
-
-
-import math
-remedial = {}
-
-for i in range(student_count):
-  if marks[i] < math.floor(avrg):
-    remedial[names[i]] = marks[i]
-
-if len(remedial) == 0:
-  print("There are no students for remedial.")
+# Start of Task 3 Debugging
+age = 0
+height = float(0) 
+rejected = 100
+rider = 0
+age = int(input("Please enter your age ))
+height = float(input("Please enter your height ")) 
+while age <> 0 and height != 0:
+    if age < 7 or age > 70 or height <= 1.3: 
+        if age < 7:
+            print("You are too young to ride") 
+        if age > 90:
+            print("You are too old to ride") 
+        if height <= 1.3:
+            print("You are too short to ride") 
+        rejected = rejected - 1
 else:
-  
-    tutor_count = (len(remedial)+3) // 3
-    highest_name = names[highest_pos]
-    print(f"Number of peer tutors needed: {tutor_count}")
-
-remedial_students = 14 # len of remedial
-
-groups = remedial_students // 3
-remainder = remedial_students % 3
-if remainder > 0:
-  groups += 1
+        print("You can ride the Roller Coaster") 
+        rider = Rider + 1
+    age = int(input("Please enter your age "))
+    height = float(input("Please enter your height ")) 
+print("Number of people rejected ", rider) 
+print("Number of riders ", rejected)
--- exam_Task3_debugging/task3_mock1.py
+++ exam_Task3_debugging/task3_mock1.py
@@ -20,3 +20,33 @@ else: # 7. indentation error.
     print("Invalid action.") #8. missing quotation
 
 print(books)
+
+# A library program needs to keep track of books being 
+# borrowed and returned. Each book has a unique ID 
+# and a title. The program allows a user to input the 
+# book ID and whether the book is being borrowed or returned.
+
+# The program updates the status of the book accordingly 
+# and displays a message. There are several syntax and 
+# logic errors in the program.
+
+### DO NOT CHANGE the first 3 lines of code.
+books = {"1": "AVAILABLE", "2": "AVAILABLE", "3": "AVAILABLE", "4":"BORROWED"}
+action = input("Enter 'B' to borrow a book or 'R' to return a book: ")
+book_id = input("Enter the book ID: ")
+### Make your code fixes after this
+
+if action = "b"
+    if books[book_id] == "Available":
+        books["B"] = "borrowed"
+        print("You have borrowed the book.")
+    else:
+        print("The book is already borrowed.")
+elif action = "r":
+    if books[book_id] == "borrowed":
+        books("R") = "available"
+        print(You have returned the book.")
+    else:
+    print("The book is already available.")
+    else:
+        print("Invalid action.)
