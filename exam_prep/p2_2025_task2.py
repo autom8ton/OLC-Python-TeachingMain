a4eaf17d07057d7225893c3da7f995674f19079a
BuildTools <unconfigured@null.spigotmc.org>
2025-10-11 13:34:50 +0800

    2025-10-11

---
+++ exam_prep/p2_2025_task2.py
@@ -0,0 +1,45 @@
+# Task 2.1 [3]
+
+# Extend the program to:
+# - ask the user to input a new word
+# - take the new word as input
+# - convert the new word to lower case
+# - store the new word at the end of the list in a new space
+
+# You do not need to consider any validation for the new word.
+
+# Save your program
+
+# ----------------------------------------------
+
+
+
+
+
+
+# ----------------------------------------------
+# Task 2.2 [4]
+
+# Copy and paste your program from sub-task 2.1
+
+# Extend your program to 
+# - search the list to find words that have 5 or more letters
+# - count and output the number of words that have five or more letters, with a suitable output message.
+
+# Save your program 
+# ----------------------------------------------
+
+
+
+
+# ----------------------------------------------
+# Task 2.3 [3]
+
+# Copy and paste your program from sub-task 2.2
+
+# Extend your program to :
+# - search the list to find words that begin and end with the same letter
+# - count and output the number of words that begin and end with the same letter, with a suitable output message.
+
+# Save your program for Task 2
+# ----------------------------------------------
