2ba965a3932c559dab9762782f10ed72af355133
DLEE <90884127+autom8ton@users.noreply.github.com>
2026-01-24 08:04:44 +0000

    2026-01-24

---
+++ exam_Task4_development/task_2018.py
@@ -0,0 +1,64 @@
+# Task 4
+# You have been asked to create a guessing game program.
+
+# The program should:
+# ·       Allow player 1 to input a whole number between 1 and 50 inclusive, for player 2 to guess. 
+#         There must be validation present to check that the number entered is between a and 50 inclusive.
+
+# ·       Allow player 2 to have 5 guesses to correctly guess the number input by player 1.
+#         You do not need to validate the input for player 2.
+
+# ·       Output “You guessed the correct number.” When player 2 inputs the same number as player 1. 
+#         The game must end if the correct number is guessed.
+
+# ·       Output “You did not guess the correct number.” When player 2 does not input the same number as player 1.
+
+# ·       Output “Game over!” when player 2 has five incorrect guesses.
+ 
+#############################################################
+# 10.  Write your program and test that it works.
+
+# Save your program as MYGUESS1_<your name>_<center number>_<index number>.py
+
+# [10]
+#################################################################
+# Write your code here
+
+################################################################
+# 11.  When your program is complete, test it for the following:
+
+# ·       Test 1 – Player 1 inputs the number 55
+# ·       Test 2 – Player 1 inputs the number 0
+# ·       Test 3 – Player 1 inputs the number 10 and player 2 guesses the numbers 15 and 10
+# ·       Test 4 – Player 1 inputs the number 20 and player 2 guesses the numbers 30, 35, 22, 15, 49
+# [4]
+###############################################################
+# Write your code here
+
+
+
+################################################################
+# 12.  Save your program as MYGUESS2_<your name>_<center number>_<index number>.py
+
+# Extend your program to identify if player 2’s guess is lower or higher than the number input by player 1. 
+# A suitable message must then be output.
+# Save your program.
+# [2]
+#################################################################
+# Write your code here
+
+
+
+#########################################################################
+# 13. 
+# Extend your program to allow player 2 to choose an easy, medium or hard game. 
+# An easy game allows eight guesses, a medium game allows six guesses and a hard game allows four guesses.
+
+# If player 2 inputs a correct guess, the program must output the number of guesses made.
+
+# You are not required to validate the input for player 2.
+
+# Save your program.
+# [4]
+#########################################################################
+# Write your code here
